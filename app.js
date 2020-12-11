c51948ca804622a0027e5495694ca5aa6d55c87c
menna15 <mennaahmedali77@gmail.com>
2020-12-12 01:09:54 +0200

    .

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app= express();
+
+
+const port= process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+
+    return res.render('Home', {
+        title: 'Home'
+
+    })
+});
+
+app.listen(port,(error)=>{
+    if(error) return console.log(error);
+    console.log(`server started listening at ${port}`);
+});
