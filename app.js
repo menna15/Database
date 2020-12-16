b240be2300bde9fc9ee0ac496926dd11a1bb799d
menna15 <mennaahmedali77@gmail.com>
2020-12-16 06:05:05 +0200

    particles animation created

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
