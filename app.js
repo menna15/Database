ead911b6271951308426ecc7201517c100d7246c
reem <reem.atalah1@gmail.com>
2020-12-28 19:53:16 +0200

    Add Courses

--- app.js
+++ app.js
@@ -30,18 +30,13 @@ app.use('/login', require('./routes/login'));
 app.use('/categories', require('./routes/categories'));
 app.use('/programs', require('./routes/programs'));
 app.use('/joinOurStaff', require('./routes/joinOurStaff'));
-<<<<<<< HEAD
 app.use('/course', require('./routes/course'));
 app.use('/singleCourse', require('./routes/singleCourse'));
 
 app.listen(port,(error)=>{
     if(error) return console.log(error);
-=======
 app.use('/Account_Settings', require('./routes/Account_Settings'));
-app.use('/Achievements', require('./routes/Achievements'));
+app.use('/Achievements', require('./routes/Achievements')) 
+console.log(`server started listening at ${port}`);
 
-app.listen(port, (error) => {
-    if (error) return console.log(error);
->>>>>>> d5bdc57b2f64f7dddb22a6bf33965e03bbb2b476
-    console.log(`server started listening at ${port}`);
 });
